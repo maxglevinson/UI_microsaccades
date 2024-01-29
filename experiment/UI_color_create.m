5b71ff464ed133a3ec1c3b0fb92e541e97b4a1f9
maxglevinson <maxglevinson@gmail.com>
2024-01-29 16:20:55 -0500

    clean color code

--- experiment/UI_color_create.m
+++ experiment/UI_color_create.m
@@ -8,26 +8,15 @@ function [fillColors, frameColors] = UI_color_create(colorCenter, colorPeriphery
 % first check if we instead just manually set the RGB values
 if isfield(params, 'centerRGBforced') % we set centerRGBforced and peripheryRGBforced in this case
     % if so, ignore colorCenter and colorPeriphery arguments
-    if colorCenter == params.colorCenter % a normal block
         center_rgb = params.centerRGBforced;
         periphery_idx = find(colorPeriphery == params.colorPeriphery);
         periphery_rgb = params.peripheryRGBforced(:, periphery_idx);
-    else % all uniform block; take average color
-        center_rgb = params.centerRGBforced;
-        periphery_rgb = params.peripheryRGBforced(:, 2);
-        center_rgb = mean([center_rgb, periphery_rgb], 2); % average them
-        periphery_rgb = center_rgb; % set center and periphery equal
-    end
 else % we didn't set them, have to calculate them from DKL values
     center_rgb = UI_dkl2rgb(params, colorCenter);
     periphery_rgb = UI_dkl2rgb(params, colorPeriphery);
 end
 
 fillColors = [periphery_rgb, center_rgb]; % PERIPHERY FIRST!
-% % gamma correct to see what it looks like
-% for i = 1:numel(fillColors)
-%     fillColors(i) = ((fillColors(i) / 255) ^ (1/2.2)) * 255;
-% end
 
 %% make the soft border
 % scale from current center & periphery colors
--- experiment/UI_color_init.m
+++ experiment/UI_color_init.m
@@ -10,7 +10,7 @@ fillRects(:, 2) = params.Central_rect; % center
 
 % create larger 'central' areas similar to how we created original center
 % these will be the framed rectangles
-fade_area_deg = params.centerdva * 2 * 0.01 * 6; % same as online study (circle radius = 4); dva of total fading area
+fade_area_deg = params.centerdva * 2 * 0.01 * 6; % dva of total fading area; comes out to 12% the radius
 thickness = round((tand(fade_area_deg)*params.cm_to_screen*params.pxbycm_y) / nframeRects); % in pixels
 frameRects = NaN(4, nframeRects);
 c = params.Central_rect;
@@ -20,18 +20,3 @@ for f = 1:nframeRects
     frameRects(3, f) = c(3) + thickness*f;
     frameRects(4, f) = c(4) + thickness*f;
 end
-
-%% if rapid frequency tagging, add more quadrants
-if params.rapidTag
-    fillRects = squeeze([params.quadrants.rect(1, :)', params.quadrants.rect(2, :)', params.quadrants.rect(3, :)', params.quadrants.rect(4, :)']);
-    for quad = 1:4
-        fillRects = [fillRects, params.quadrants.center(quad, :)']; % peripheries first, then centers
-        if quad ~= 1
-        for f = 1:nframeRects
-            frameRects = [frameRects, CenterRect(frameRects(:, f)', params.quadrants.rect(quad, :))'];
-        end
-        end
-    end
-end
-
-%% IMPORTANT NOTE: MIGHT NEED TO TRY LOWERING # OF FRAMED RECTS IF DISPLAY CANNOT FLIP IN TIME %%
